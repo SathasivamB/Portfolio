85438e761cbd5cc114079a7111e27ad8793ec784
SathasivamB <sathasivamb95@gmail.com>
2024-08-10 14:55:46 +0530

    Add files via upload

---
+++ src/utils/data.js
@@ -0,0 +1,71 @@
+export const SKILLS = [
+    {
+        title: "Frontend",
+        icon: "../../src/assets/images/icons8-frontend-64.png",
+        skills: [
+            {skill: "HTMLS", percentage: "90%"},
+            {skill: "CSS3", percentage: "80%"},
+            {skill: "JavaScript", percentage: "75%"},
+            {skill: "React.js", percentage: "85%"},
+        ]
+    },
+    {
+        title: "Backend",
+        icon: "../../src/assets/images/icons8-backend-development-64.png",
+        skills: [
+            {skill: "Node.js", percentage: "70%"},
+            {skill: "Express.js", percentage: "65%"}
+        ],
+    },
+    {
+        title: "Tools",
+        icon: "../../src/assets/images/icons8-tools-64.png",
+        skills: [
+            {skill: "Git & GitHub", percentage: "85%"},
+            {skill: "Visual Studio Code", percentage: "90%"},
+            {skill: "Webpack", percentage: "60%"},
+            {skill: "Responsive Design", percentage: "75%"},
+        ],
+    },
+    {
+        title: "Soft Skills",
+        icon: "../../src/assets/images/icons8-learning-64.png",
+        skills: [
+            {skill: "Problem-solving", percentage: "80%"},
+            {skill: "Collaboration", percentage: "85%"},
+            {skill: "Attention to Detail", percentage: "75%"},
+        ],
+    },
+];
+
+export const WORK_EXPERIENCE = [
+    {
+        title: "Having Some Experience in React.js",
+        date: "June 2024 - Aug 2024( Present )",
+        responsibilties: [
+            "Learned Frontend using Youtube by watching various videos.",
+            "Now trying to learn Backend for future use.",
+        ]
+
+    },
+
+    {
+        title: "Started as a Newbie in React.js",
+        date: "March 2024 - May 2024",
+        responsibilties: [
+            "Learned Frontend using Youtube by watching various videos.",
+            "Now trying to learn Backend for future use.",
+        ]
+
+    },
+
+    {
+        title: "Had an Idea to Create a Website using React.js",
+        date: "Jan 2024 - Feb 2024",
+        responsibilties: [
+            "Learned Frontend using Youtube by watching various videos.",
+            "Now trying to learn Backend for future use.",
+        ]
+
+    }
+]
